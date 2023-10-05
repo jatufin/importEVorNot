a383740153ef08ecab76aac3fd721ecdd6ffa206
Toni Rämö <toni.ramo@helsinki.fi>
2023-10-05 12:18:19 +0300

    Refactor parser

--- mobile_de/mobilede_parser.py
+++ mobile_de/mobilede_parser.py
@@ -20,17 +20,12 @@ def extract_car_data(html):
     data["power_kw"] = re.findall(r"\d+", soup.find(id="power-v").get_text())[0]
     data["capacity_kwh"] = _parse_number(s.get_text()) if (s:= soup.find(id="batteryCapacity-v")) else None
     data["automatic"] = s.get_text() == "Automatik" if (s:=soup.find(id="transmission-v")) else None
-    data["undamaged"] = s.get_text() == "Unfallfrei" if  (s := soup.find(id="damageCondition-v")) else None
-
-    data["el_cons_kwh100km"] = None
-    if (s:= soup.find(id="envkv.wltp.powerConsumption-v")):
-        data["el_cons_kwh100km"] = (".").join(re.findall(r"(\d+),(\d+)", s.get_text())[0])
-    
-    data["n_prev_owners"] = None
-    if (s:= (soup.find("div", {"class" : "key-feature key-feature--numberOfPreviousOwners"})
-                 .find("div", {"class" : "key-feature__value"}))):
-        data["n_prev_owners"] = s.get_text()
-    
+    data["undamaged"] = s.get_text() == "Unfallfrei" if (s := soup.find(id="damageCondition-v")) else None
+    s = soup.find(id="envkv.wltp.powerConsumption-v")
+    data["el_cons_kwh100km"] = (".").join(re.findall(r"(\d+),(\d+)", s.get_text())[0]) if s else None
+    s = (soup.find("div", {"class" : "key-feature key-feature--numberOfPreviousOwners"})
+             .find("div", {"class" : "key-feature__value"}))
+    data["n_prev_owners"] = s.get_text() if s else None
     data["features"] = [item.get_text() for item in soup.find(id="features").find_all("div", {"class" : "bullet-list"})]
     if (s:= soup.find(id="parkAssists-v")):
         data["features"] = data["features"] + list(map(lambda x: f"Parking sensor: {x.strip()}", s.get_text().split(",")))
