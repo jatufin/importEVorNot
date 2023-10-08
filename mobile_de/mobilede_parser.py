86767213b7c473f4d98fd3ab0c28536badff265c
Toni Rämö <toni.ramo@helsinki.fi>
2023-10-08 17:57:39 +0300

    update parser

--- mobile_de/mobilede_parser.py
+++ mobile_de/mobilede_parser.py
@@ -24,22 +24,25 @@ def extract_car_data(html):
     soup = BeautifulSoup(page_n, 'html.parser')
 
     data = {}
-    data["brand"], data["model"] = soup.find(id="ad-title").get_text().split(" ", 1)
-    data["color"], data["color_type"], *_ =  s.get_text().split() + ["Nonmetallic"] if (s:=soup.find(id="color-v")) else [None, None]
+    data["make"], data["model"] = soup.find(id="ad-title").get_text().split(" ", 1)
+    data["color"], color_type, *_ = s.get_text().split() + ["Nonmetallic"] if (s:=soup.find(id="color-v")) else [None, None]
+    data["metallicColor"] = "Metallic" in color_type
     data["kilometers"] = _parse_number(soup.find(id="mileage-v").get_text())
     data["price"] = _parse_number(soup.find("span", {"data-testid" : "prime-price"}).get_text())
     data["registration"] = s.get_text() if (s:=soup.find(id="firstRegistration-v")) else None
     data["electric"] = soup.find(id="fuel-v").get_text() == "Elektro"
-    data["power_kw"] = re.findall(r"\d+", soup.find(id="power-v").get_text())[0]
-    data["capacity_kwh"] = _get_capacity(soup)
+    data["power"] = re.findall(r"\d+", soup.find(id="power-v").get_text())[0]
+    data["batteryCapacity"] = _get_capacity(soup)
     data["automatic"] = s.get_text() == "Automatik" if (s:=soup.find(id="transmission-v")) else None
     data["undamaged"] = s.get_text() == "Unfallfrei" if (s := soup.find(id="damageCondition-v")) else None
-    data["n_seats"] = s.get_text() if (s:= soup.find(id="numSeats-v")) else None
+    data["seats"] = s.get_text() if (s:= soup.find(id="numSeats-v")) else None
     data["leather_upholstery"] = "Vollleder" in s.get_text() if (s:= soup.find(id="interior-v")) else None  
-      
+    data["isSUV"] = ("SUV" in s.get_text()) if (s:= soup.find(id="category-v")) else None
+    data["airconditioning"] = ("Keine Klimaanlage" not in s.get_text()) if (s:= soup.find(id="climatisation-v")) else None
+
     s= soup.find(id="envkv.wltp.powerConsumption-v")
     res = re.findall(r"(\d+,\d+)", s.get_text()) if s else None
-    data["el_cons_kwh100km"] = res[0].replace(",", ".") if res and len(res) else None
+    data["elec_cons"] = res[0].replace(",", ".") if res and len(res) else None
 
     s = soup.find("div", {"class" : "key-feature key-feature--numberOfPreviousOwners"})
     s = s.find("div", {"class" : "key-feature__value"}) if s else None
